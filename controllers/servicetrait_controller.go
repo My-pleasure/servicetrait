4346d5a219882d5e4a18e78b8a850c9af2b3de73
My-pleasure <dearqwq@qq.com>
2020-05-23 11:15:22 +0800

    Change version to v1alpha2

---
+++ controllers/servicetrait_controller.go
@@ -0,0 +1,190 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	cpv1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+	"github.com/crossplane/oam-controllers/pkg/oam/util"
+	"github.com/crossplane/oam-kubernetes-runtime/pkg/oam"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	corev1alpha2 "servicetrait/api/v1alpha2"
+)
+
+const (
+	oamReconcileWait = 30 * time.Second
+)
+
+// Reconcile error strings.
+const (
+	errLocateWorkload    = "cannot find workload"
+	errLocateResources   = "cannot find resources"
+	errUpdateStatus      = "cannot apply status"
+	errLocateStatefulSet = "cannot find statefulset"
+	errApplyService      = "cannot apply the service"
+	errGCService         = "cannot clean up stale services"
+)
+
+// ServiceTraitReconciler reconciles a ServiceTrait object
+type ServiceTraitReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=core.oam.dev,resources=servicetraits,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core.oam.dev,resources=servicetraits/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core.oam.dev,resources=statefulsetworkloads,verbs=get;list;
+// +kubebuilder:rbac:groups=core.oam.dev,resources=statefulsetworkloads/status,verbs=get;
+// +kubebuilder:rbac:groups=core.oam.dev,resources=workloaddefinition,verbs=get;list;
+// +kubebuilder:rbac:groups=apps,resources=statefulset,verbs=get;list;watch;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+
+func (r *ServiceTraitReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("servicetrait", req.NamespacedName)
+	log.Info("Reconcile Service Trait")
+
+	var service corev1alpha2.ServiceTrait
+	if err := r.Get(ctx, req.NamespacedName, &service); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	log.Info("Get the service trait", "WorkloadReference", service.Spec.WorkloadReference)
+
+	// Fetch the workload this trait is referring to
+	workload, result, err := r.fetchWorkload(ctx, &service)
+	if err != nil {
+		return result, err
+	}
+
+	// Fetch the child resources list from the corresponding workload
+	resources, err := util.FetchWorkloadDefinition(ctx, r, workload)
+	if err != nil {
+		r.Log.Error(err, "Cannot find the workload child resources", "workload", workload.UnstructuredContent())
+		service.Status.SetConditions(cpv1alpha1.ReconcileError(fmt.Errorf(errLocateResources)))
+		return ctrl.Result{RequeueAfter: oamReconcileWait}, errors.Wrap(r.Status().Update(ctx, &service),
+			errUpdateStatus)
+	}
+
+	// Create a service for the child resources we know
+	svc, err := r.createService(ctx, service, resources)
+	if err != nil {
+		return result, err
+	}
+
+	// server side apply the service, only the fields we set are touched
+	applyOpts := []client.PatchOption{client.ForceOwnership, client.FieldOwner(service.Name)}
+	if err := r.Patch(ctx, svc, client.Apply, applyOpts...); err != nil {
+		service.Status.SetConditions(cpv1alpha1.ReconcileError(errors.Wrap(err, errApplyService)))
+		r.Log.Error(err, "Failed to apply a service")
+		return reconcile.Result{RequeueAfter: oamReconcileWait}, errors.Wrap(r.Status().Update(ctx, &service),
+			errUpdateStatus)
+	}
+	r.Log.Info("Successfully applied a service", "UID", svc.UID)
+
+	// garbage collect the service that we created but not needed
+	if err := r.cleanupResources(ctx, &service, &svc.UID); err != nil {
+		service.Status.SetConditions(cpv1alpha1.ReconcileError(errors.Wrap(err, errGCService)))
+		r.Log.Error(err, "Failed to clean up resources")
+		return reconcile.Result{RequeueAfter: oamReconcileWait}, errors.Wrap(r.Status().Update(ctx, &service),
+			errUpdateStatus)
+	}
+	service.Status.Resources = nil
+	// record the new service
+	service.Status.Resources = append(service.Status.Resources, cpv1alpha1.TypedReference{
+		APIVersion: svc.GetObjectKind().GroupVersionKind().GroupVersion().String(),
+		Kind:       svc.GetObjectKind().GroupVersionKind().Kind,
+		Name:       svc.GetName(),
+		UID:        svc.GetUID(),
+	})
+
+	service.Status.SetConditions(cpv1alpha1.ReconcileSuccess())
+
+	return ctrl.Result{}, errors.Wrap(r.Status().Update(ctx, &service), errUpdateStatus)
+}
+
+func (r *ServiceTraitReconciler) createService(ctx context.Context, service corev1alpha2.ServiceTrait,
+	resources []*unstructured.Unstructured) (*corev1.Service, error) {
+	found := false
+	// Change unstructured to object
+	for _, res := range resources {
+		if res.GetKind() == KindStatefulSet && res.GetAPIVersion() == appsv1.SchemeGroupVersion.String() {
+			found = true
+			r.Log.Info("Get the statefulset the trait is going to create a service for it",
+				"statefulset name", res.GetName(), "UID", res.GetUID())
+			// convert the unstructured to statefulset and create a service
+			var ss appsv1.StatefulSet
+			bts, _ := json.Marshal(res)
+			if err := json.Unmarshal(bts, &ss); err != nil {
+				r.Log.Error(err, "Failed to convert an unstructured obj to a statefulset")
+				continue
+			}
+			// Create a service for the workload which this trait is referring to
+			svc, err := r.renderService(ctx, &service, &ss)
+			if err != nil {
+				r.Log.Error(err, "Failed to render a service")
+				return nil, errors.Wrap(r.Status().Update(ctx, &service),
+					errUpdateStatus)
+			}
+			return svc, nil
+		}
+	}
+	if !found {
+		r.Log.Info("Cannot locate any statefulset", "total resources", len(resources))
+		service.Status.SetConditions(cpv1alpha1.ReconcileError(fmt.Errorf(errLocateStatefulSet)))
+		return nil, errors.Wrap(r.Status().Update(ctx, &service),
+			errUpdateStatus)
+	}
+	return nil, nil
+}
+
+func (r *ServiceTraitReconciler) fetchWorkload(ctx context.Context,
+	oamTrait oam.Trait) (*unstructured.Unstructured, ctrl.Result, error) {
+	var workload unstructured.Unstructured
+	workload.SetAPIVersion(oamTrait.GetWorkloadReference().APIVersion)
+	workload.SetKind(oamTrait.GetWorkloadReference().Kind)
+	wn := client.ObjectKey{Name: oamTrait.GetWorkloadReference().Name, Namespace: oamTrait.GetNamespace()}
+	if err := r.Get(ctx, wn, &workload); err != nil {
+		oamTrait.SetConditions(cpv1alpha1.ReconcileError(errors.Wrap(err, errLocateWorkload)))
+		r.Log.Error(err, "Workload not find", "kind", oamTrait.GetWorkloadReference().Kind,
+			"workload name", oamTrait.GetWorkloadReference().Name)
+		return nil, ctrl.Result{RequeueAfter: oamReconcileWait}, errors.Wrap(r.Status().Update(ctx, oamTrait),
+			errUpdateStatus)
+	}
+	r.Log.Info("Get the workload the trait is pointing to", "workload name", oamTrait.GetWorkloadReference().Name,
+		"UID", workload.GetUID())
+	return &workload, ctrl.Result{}, nil
+}
+
+func (r *ServiceTraitReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1alpha2.ServiceTrait{}).
+		Complete(r)
+}
---
+++ pkg/trait/service/service.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"context"
+	"github.com/crossplane/oam-kubernetes-runtime/pkg/oam"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"reflect"
+)
+
+// Reconcile error strings.
+const (
+	errNotServiceTrait = "object is not a service trait"
+)
+
+const LabelKey = "workload.oam.crossplane.io"
+
+var (
+	serviceKind       = reflect.TypeOf(corev1.Service{}).Name()
+	serviceAPIVersion = corev1.SchemeGroupVersion.String()
+)
+
+// ServiceInjector adds a Service object for the first Port on the first
+// Container for the first Deployment observed in a workload translation.
+func ServiceInjector(ctx context.Context, t oam.Trait, objs []oam.Object) ([]oam.Object, error) {
+	if objs == nil {
+		return nil, nil
+	}
+
+	for _, o := range objs {
+		set, ok := o.(*appsv1.StatefulSet)
+		if !ok {
+			continue
+		}
+
+		// We don't add a Service if there are no containers for the StatefulSet.
+		// This should never happen in practice.
+		if len(set.Spec.Template.Spec.Containers) < 1 {
+			continue
+		}
+		svc := &corev1.Service{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       serviceKind,
+				APIVersion: serviceAPIVersion,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      set.GetName(),
+				Namespace: set.GetNamespace(),
+				Labels: map[string]string{
+					LabelKey: string(t.GetUID()),
+				},
+			},
+			Spec: corev1.ServiceSpec{
+				Selector: set.Spec.Selector.MatchLabels,
+				Ports:    []corev1.ServicePort{},
+				Type:     corev1.ServiceTypeClusterIP,
+			},
+		}
+
+		// We only add a single Service for the StatefulSet, even if multiple
+		// ports or no ports are defined on the first container. This is to
+		// exclude the need for implementing garbage collection in the
+		// short-term in the case that ports are modified after creation.
+		if len(set.Spec.Template.Spec.Containers[0].Ports) > 0 {
+			svc.Spec.Ports = []corev1.ServicePort{
+				{
+					Name:       set.GetName(),
+					Port:       set.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort,
+					TargetPort: intstr.FromInt(int(set.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort)),
+				},
+			}
+		}
+		objs = append(objs, svc)
+		break
+	}
+
+	return objs, nil
+}
